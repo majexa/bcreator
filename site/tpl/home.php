88e279a16a459590feffab0d5ff5aa3be846cf78
masted <masted311@gmail.com>
2016-04-25 13:38:58 +0300

    ...

--- site/tpl/home.php
+++ site/tpl/home.php
@@ -2,12 +2,6 @@
   Logged in as <?= Auth::get('name') ?>, <?= Auth::get('email') ?>. <a href="?logout=1">Logout</a>
 <? } ?>
 
-<ul class="menu">
-  <li><a href="/list/create">Create New</a></li>
-  <li><a href="/list">My banners</a></li>
-  <li><a href="/profile">Profile</a></li>
-</ul>
-
 <div class="apeform">
   <h2>Sign In</h2>
   <?= $d['signInForm'] ?>
--- site/tpl/main.php
+++ site/tpl/main.php
@@ -4,6 +4,11 @@
   <? if (!strstr($d['ctrlName'], 'Cpanel')) { ?>
     <div style="padding: 10px 50px;">
       <p><a href="/"><img src="/m/img/logo.png"></a></p>
+      <ul class="menu">
+        <li><a href="/list/create">Create New</a></li>
+        <li><a href="/list">My banners</a></li>
+        <li><a href="/profile">Profile</a></li>
+      </ul>
       <? $this->tpl($d['tpl'], $d) ?>
     </div>
   <? }
