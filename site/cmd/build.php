23d0fa2abf88ff69edae0a690f9f7ac1d5624afd
masted <masted311@gmail.com>
2016-06-01 12:21:38 +0300

    add build file

---
+++ site/cmd/build.php
@@ -0,0 +1,6 @@
+<?php
+
+file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11');
+file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/78');
+file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/84');
+print "done\n";
