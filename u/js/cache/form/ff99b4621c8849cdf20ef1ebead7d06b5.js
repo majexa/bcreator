5351a630a8b5938140b42c217577eb3e8d3c67ba
masted <masted311@gmail.com>
2016-06-02 19:28:37 +0300

    Prevent editing of non-editable layers on layer click

---
+++ u/js/cache/form/ff99b4621c8849cdf20ef1ebead7d06b5.js
@@ -0,0 +1,18 @@
+Ngn.Frm.init.ff99b4621c8849cdf20ef1ebead7d06b5 = function() {
+
+var id = 'ff99b4621c8849cdf20ef1ebead7d06b5';
+Ngn.Form.forms[id].eForm.getElements('.type_fieldList').each(function(el){
+  new Ngn.Frm.FieldSet(Ngn.Form.forms[id], el, {'rowElementSelector': '.hgrp'});
+});
+
+// -- type: color --
+new Ngn.Form.ElInit.factory(Ngn.Form.forms.ff99b4621c8849cdf20ef1ebead7d06b5, 'color');
+
+// -- type: fontFamilyCufon --
+new Ngn.Form.ElInit.factory(Ngn.Form.forms.ff99b4621c8849cdf20ef1ebead7d06b5, 'fontFamilyCufon');
+
+// -- jsMaxLength -- 
+
+Ngn.Frm.maxLength($('ff99b4621c8849cdf20ef1ebead7d06b5'), 1000);
+
+};
