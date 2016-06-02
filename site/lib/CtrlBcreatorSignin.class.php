9214f3c55d9e0c40890459df189754c9a56bd193
masted <masted311@gmail.com>
2016-06-02 16:40:00 +0300

    redirect to home on signin page

--- site/lib/CtrlBcreatorSignin.class.php
+++ site/lib/CtrlBcreatorSignin.class.php
@@ -4,13 +4,13 @@ class CtrlBcreatorSignin extends CtrlBcreatorLanding {
 
   function action_default() {
     if (Auth::get('id')) {
-      $this->redirect('/profile');
+      $this->redirect('/');
       return;
     }
     $form = BcreatorCore::extendForm(new AuthForm);
     $form->fields->fields['authPass']['help'] = '<a href="/resetPassword" class="reset-pass">Reset Password</a>';
     if ($form->isSubmittedAndValid()) {
-      $this->redirect('/profile');
+      $this->redirect('/');
     }
     $this->d['form'] = $form->html();
     $this->d['innerTpl'] = 'landing/form';
