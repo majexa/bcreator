f4cb4900be92dea5bc1377955f6a5a5f2a20dd7a
masted <masted311@gmail.com>
2016-06-02 16:57:33 +0300

    Fix gray & white dialogs style conflict

---
+++ u/js/cache/form/fd6ad800f69e5906b6ae8c9ddb8507084.js
@@ -0,0 +1,13 @@
+Ngn.Frm.init.fd6ad800f69e5906b6ae8c9ddb8507084 = function() {
+Ngn.Form.elOptions.image = {"deleteImageUrl":"\/pageBlock\/6\/json_deleteImage\/85\/{n}"}
+
+var id = 'fd6ad800f69e5906b6ae8c9ddb8507084';
+Ngn.Form.forms[id].eForm.getElements('.type_fieldListBcreatorImages').each(function(el){
+  new Ngn.Frm.FieldSetBcreatorImages(Ngn.Form.forms[id], el, {'deleteImageUrl': '/pageBlock/6/json_deleteImage/85/{n}', 'rowElementSelector': '.hgrp'});
+});
+
+// -- jsMaxLength -- 
+
+Ngn.Frm.maxLength($('fd6ad800f69e5906b6ae8c9ddb8507084'), 1000);
+
+};
