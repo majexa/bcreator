ac40d21096c9cb7d6d9001447f5de3ad149fdaa1
masted <masted311@gmail.com>
2016-06-16 17:20:23 +0300

    ...

---
+++ u/js/cache/form/f5caf224441386422ac4387785468ef49.js
@@ -0,0 +1,7 @@
+Ngn.Frm.init.f5caf224441386422ac4387785468ef49 = function() {
+
+// -- jsMaxLength -- 
+
+Ngn.Frm.maxLength($('f5caf224441386422ac4387785468ef49'), 1000);
+
+};
---
+++ u/js/cache/form/ff4e7daaa247d8b36ae2a8b9c4da2cbac.js
@@ -0,0 +1,7 @@
+Ngn.Frm.init.ff4e7daaa247d8b36ae2a8b9c4da2cbac = function() {
+
+// -- jsMaxLength -- 
+
+Ngn.Frm.maxLength($('ff4e7daaa247d8b36ae2a8b9c4da2cbac'), 1000);
+
+};
