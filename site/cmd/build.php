61a0a2aeaa96ce555dc16b2851287b992e863dfe
masted <masted311@gmail.com>
2016-06-17 15:42:41 +0300

    ..

--- site/cmd/build.php
+++ site/cmd/build.php
@@ -1,8 +1,7 @@
 <?php
 
-//die2('http://'.SITE_DOMAIN.'/cpanel/11');
-
 file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11?renderKey='.Config::getVar('sd/renderKey'));
+file_get_contents('http://'.SITE_DOMAIN.'/newBanner');
 file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/78');
 file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/84');
 file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11/json_settings');
