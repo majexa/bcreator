6eec581cf113aee650ff8fe92c9c66f385f01385
masted <masted311@gmail.com>
2016-06-17 14:33:09 +0300

    ...

--- site/cmd/build.php
+++ site/cmd/build.php
@@ -5,4 +5,5 @@
 file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11?renderKey='.Config::getVar('sd/renderKey'));
 file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/78');
 file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/84');
+file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11/json_settings');
 print "done\n";
