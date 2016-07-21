7ca382968480a78f42af0203962abf64e827afea
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-07-21 12:32:46 +0300

    build fixes

--- site/cmd/build.php
+++ site/cmd/build.php
@@ -3,10 +3,11 @@
 $currentBuildMode = BUILD_MODE;
 ProjectConfig::updateConstant('more', 'BUILD_MODE', true);
 print `php cmd.php cc`;
-file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11?renderKey='.Config::getVar('sd/renderKey'));
+
+file_get_contents('http://'.SITE_DOMAIN.'/cpanel/12?renderKey='.Config::getVar('sd/renderKey'));
 file_get_contents('http://'.SITE_DOMAIN.'/newBanner');
-file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/78');
-file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/84');
+file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/80');
+file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/142');
 file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11/json_settings');
 if ($currentBuildMode === false) ProjectConfig::updateConstant('more', 'BUILD_MODE', false);
 output('build complete');
---
+++ u/js/cache/form/fe020aa64deb603e7048da81848df258b.js
@@ -0,0 +1,13 @@
+Ngn.Frm.init.fe020aa64deb603e7048da81848df258b = function() {
+Ngn.Form.elOptions.image = {"deleteImageUrl":"\/pageBlock\/12\/json_deleteImage\/142\/{n}"}
+
+var id = 'fe020aa64deb603e7048da81848df258b';
+Ngn.Form.forms[id].eForm.getElements('.type_fieldListBcreatorImages').each(function(el){
+  new Ngn.sd.FieldSetAnimatedImages(Ngn.Form.forms[id], el, {'deleteImageUrl': '/pageBlock/12/json_deleteImage/142/{n}', 'rowElementSelector': '.hgrp'});
+});
+
+// -- jsMaxLength -- 
+
+Ngn.Frm.maxLength($('fe020aa64deb603e7048da81848df258b'), 1000);
+
+};
