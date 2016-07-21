f866be5247709afa13cc47a60a69edb541fe8d86
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-07-21 14:03:47 +0300

    BC-25 add build path for /support section

--- site/cmd/build.php
+++ site/cmd/build.php
@@ -6,6 +6,7 @@ print `php cmd.php cc`;
 
 file_get_contents('http://'.SITE_DOMAIN.'/cpanel/12?renderKey='.Config::getVar('sd/renderKey'));
 file_get_contents('http://'.SITE_DOMAIN.'/newBanner');
+file_get_contents('http://'.SITE_DOMAIN.'/support');
 file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/80');
 file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/11/json_blockSettings/142');
 file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11/json_settings');
---
+++ u/js/cache/form/fdf60153071b58a8b54b052c1ae111897.js
@@ -0,0 +1,10 @@
+Ngn.Frm.init.fdf60153071b58a8b54b052c1ae111897 = function() {
+
+// -- type: textarea --
+new Ngn.Form.ElInit.factory(Ngn.Form.forms.fdf60153071b58a8b54b052c1ae111897, 'textarea');
+
+// -- jsMaxLength -- 
+
+Ngn.Frm.maxLength($('fdf60153071b58a8b54b052c1ae111897'), 1000);
+
+};
