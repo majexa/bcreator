28e1c22a84f9c998881e7f5c481cce171293460b
bogdik <bogdikxxx@gmail.com>
2016-08-31 12:22:02 +0300

    Fix 2 issue BC-101

--- site/tpl/inner.php
+++ site/tpl/inner.php
@@ -1,16 +1,17 @@
 <div class="profileBar">
   <div class="profileBarBg"></div>
   <div class="profileBarBody">
-    <div class="login">
+    <!--<div class="login">
       <div class="avatar"></div>
       <div class="name"><?= Auth::get('name') ?></div>
       <div class="email"><?= Misc::cut(Auth::get('email'), 10) ?></div>
-    </div>
+    </div>-->
     <!--<div class="dropdown">-->
-      <a href="/profile" class="profile"><i></i><span>My Profile</span></a>
+    <div style="margin-top: 8px; margin-left: 55px;">
+      <!--<a href="/profile" class="profile"><i></i><span>My Profile</span></a>-->
       <a href="/list" class="list"><i></i><span>My Banners</span></a>
       <a href="/?logout=1" class="logout"><i></i><span>Logout</span></a>
-    <!--</div>-->
+    </div>
   </div>
 </div>
 <script>
