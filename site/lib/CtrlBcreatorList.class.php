4d2d404c5366be60580b3db9f63c94c25a1094ef
bogdik <bogdikxxx@gmail.com>
2016-09-06 13:01:14 +0300

    Fix 2 issue BC-103

--- site/lib/CtrlBcreatorList.class.php
+++ site/lib/CtrlBcreatorList.class.php
@@ -14,6 +14,7 @@ class CtrlBcreatorList extends CtrlBcreatorLanding {
     Sflm::frontend('css')->addLib('i/css/common/tips.css');
     $this->setPageTitle('My Banners');
     $this->d['banners'] = [];
+    $this->d['banners_trash_count'] = db()->selectCell('SELECT COUNT(*) FROM bcBanners_trash WHERE userId=?d', Auth::get('id'));
     foreach (db()->select('SELECT * FROM bcBanners WHERE userId=?d', Auth::get('id')) as $v) {
       $this->d['banners'][] = new BcBanner($v);
     }
@@ -34,6 +35,33 @@ class CtrlBcreatorList extends CtrlBcreatorLanding {
     $this->d['innerTpl'] = 'landing/form';
   }
 
+  function action_getTrash() {
+      self::checkUser();
+      $trash=db()->select('SELECT * FROM bcBanners_trash WHERE userId=?d', Auth::get('id'));
+      if(count($trash)>0) {
+          echo "<table class='table' style='width: 100%; padding: 15px; display: inline-table;'><thead><tr><th>Title</th><th>Size</th><th>Actions</th></tr></thead><tbody>";
+          foreach ($trash as $v) {
+              $v = new BcBanner($v);
+              if ($v['directLink']) {
+                  $img = "<img src = " . $v['directLink'] . '?' . strtotime($v['dateRender']) . " class='thumb' >";
+              } else {
+                  $img = "";
+              }
+              echo "<tr><td><b id='name-$v[id]'>$v[title]</b><br>$img</td><td>$v[size]</td><td><span onclick='restoreTrash(\"$v[id]\");'>Restore</span> <span onclick='delTrash(\"$v[id]\");'>Delete</span></td></tr>";
+          }
+          echo "</tbody></table>";
+          exit;
+      } else {echo "No banners in trash"; exit;}
+    }
+
+    function action_getCountTrash() {
+        self::checkUser();
+        $CounTrash=db()->selectCell('SELECT Count(*) FROM bcBanners_trash WHERE userId=?d', Auth::get('id'));
+        echo $CounTrash;
+        exit;
+    }
+
+
   function action_delete() {
     self::checkUser();
     $this->d['banner'] = Misc::checkEmpty(db()->selectRow('SELECT * FROM bcBanners WHERE id=?d AND userId=?d', $this->req->param(2), Auth::get('id')));
@@ -43,10 +71,30 @@ class CtrlBcreatorList extends CtrlBcreatorLanding {
 
   function action_deleteConfirmed() {
     self::checkUser();
-    db()->query('DELETE FROM bcBanners WHERE id=?d AND userId=?d', $this->req->param(2), Auth::get('id'));
+      db()->query('INSERT INTO `bcBanners_trash` SELECT * FROM `bcBanners` WHERE `id`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+      db()->query('INSERT INTO `bcBlocks_trash` SELECT * FROM `bcBlocks` WHERE `bannerId`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+      db()->query('DELETE FROM `bcBanners` WHERE `id`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+      db()->query('DELETE FROM `bcBlocks` WHERE `bannerId`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
     $this->redirect('/list');
   }
 
+  function action_deleteConfirmedTrash() {
+        self::checkUser();
+        db()->query('DELETE FROM `bcBanners_trash` WHERE `id`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+        db()->query('DELETE FROM `bcBlocks_trash` WHERE `bannerId`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+        echo "Ok";
+      exit;
+  }
+
+  function action_restoreTrash() {
+        self::checkUser();
+        db()->query('INSERT INTO `bcBanners` SELECT * FROM `bcBanners_trash` WHERE `id`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+        db()->query('INSERT INTO `bcBlocks` SELECT * FROM `bcBlocks_trash` WHERE `bannerId`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+        db()->query('DELETE FROM `bcBanners_trash` WHERE `id`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+        db()->query('DELETE FROM `bcBlocks_trash` WHERE `bannerId`=?d AND `userId`=?d', $this->req->param(2), Auth::get('id'));
+        echo "Ok";
+      exit;
+    }
   function action_download() {
     self::checkUser();
     $banner = new BcBanner(db()->selectRow('SELECT * FROM bcBanners WHERE id=?d AND userId=?d', $this->req->param(2), Auth::get('id')));
--- site/tpl/landing/list.php
+++ site/tpl/landing/list.php
@@ -1,18 +1,14 @@
-<?/*
+<div class="center">
   <div class="subtitle">
     Create your own banners, Build, publish.
     Try Banner Creator for free Check out features
   </div>
-  */
-?>
-<style>
-  .n_list .content h1 {
-    float: left;
-
-  }
-</style>
-<div class="tableButton">
-  <a href="/list/create" class="btn">Create New Banner</a>
+  <div class="button">
+    <a href="/list/create" class=" btn">Create New Banner</a>
+    <? if ($d['banners_trash_count']>0) { ?>
+      <a href="#" class="btn" id="trash">Restore from trash <span id="trash_ctn"><? echo $d["banners_trash_count"];?></span></a>
+    <?}?>
+  </div>
 </div>
 
 <div class="banners left">
@@ -21,8 +17,10 @@
 <div class="banners right">
   <img src="http://zukulbannercreator.net/u/banner/animated/result/48.gif?qAVm2K0affRIK7a5ddbA">
 </div>
-
-<? if ($d['banners']) { ?>
+<link rel="stylesheet" type="text/css" href="/i/css/common/dialog.css">
+<link rel="stylesheet" type="text/css" href="/sd/css/list.css">
+<div class="center">
+  <? if ($d['banners']) { ?>
   <div class="table">
     <table>
       <tr>
@@ -52,63 +50,131 @@
               Need to render
             <? } ?>
           </td>
-          <td class="tool">
-            <? if ($v['downloadLink']) { ?>
-              <a href="<?= $v['downloadLink'] ?>"><img src="/m/img/landing/btn/download.png" title="Download"> Download</a>
-            <? } else { ?>
-              Never rendered
-            <? } ?>
+          <td>
+            <a href="<?= $v['downloadLink'] ?>"><img src="/m/img/landing/btn/download.png" title="Download"></a>
           </td>
           <td>
             <a href="/cpanel/<?= $v['id'] ?>"><img src="/m/img/landing/btn/edit.png" title="Edit"></a>
           </td>
           <td>
-            <img src="/m/img/landing/btn/delete.png" class="del" title="Delete" data="<?= $v['id'] ?>">
+	        <img src="/m/img/landing/btn/delete.png" class="del" title="Delete" data="<?= $v['id'] ?>">
           </td>
         </tr>
       <? } ?>
     </table>
-  </div>
-  <link rel="stylesheet" type="text/css" href="/i/css/common/dialog.css">
-  <link rel="stylesheet" type="text/css" href="/sd/css/list.css">
-  <script>
-    document.getElements('.table .preview img').each(function(el) {
-      var timeoutId = null;
-      var eFullPreview = el.getParent().getElement('.fullPreview');
-      el.addEvent('mouseover', function(e) {
-        clearTimeout(timeoutId);
-        eFullPreview.set('html', '<img src="' + el.get('src') + '">');
-        eFullPreview.setStyle('display', 'block');
+
+    <script>
+      document.getElements('.table .preview img').each(function(el) {
+        var timeoutId = null;
+        var eFullPreview = el.getParent().getElement('.fullPreview');
+        el.addEvent('mouseover', function(e) {
+          clearTimeout(timeoutId);
+          eFullPreview.set('html', '<img src="' + el.get('src') + '">');
+          eFullPreview.setStyle('display', 'block');
+        });
+        eFullPreview.addEvent('mouseover', function(e) {
+          clearTimeout(timeoutId);
+        });
+        eFullPreview.addEvent('mouseout', function(e) {
+          timeoutId = setTimeout(function() {
+            el.getParent().getElement('.fullPreview').setStyle('display', 'none');
+          }, 100);
+        });
+        el.addEvent('mouseout', function(e) {
+          timeoutId = setTimeout(function() {
+            el.getParent().getElement('.fullPreview').setStyle('display', 'none');
+          }, 100);
+        });
       });
-      eFullPreview.addEvent('mouseover', function(e) {
-        clearTimeout(timeoutId);
-      });
-      eFullPreview.addEvent('mouseout', function(e) {
-        timeoutId = setTimeout(function() {
-          el.getParent().getElement('.fullPreview').setStyle('display', 'none');
-        }, 100);
-      });
-      el.addEvent('mouseout', function(e) {
-        timeoutId = setTimeout(function() {
-          el.getParent().getElement('.fullPreview').setStyle('display', 'none');
-        }, 100);
-      });
-    });
-    new Tips('.table img');
-        $$('.del').addEvent('click', function(event) {
+      new Tips('.table img');
+      $$('.del').addEvent('click', function(event) {
           var btName = $(this).getParent().getParent().getFirst().getElement('p').get('text');
-          var dialog = new Ngn.Dialog.Confirm({
-            message: "Are you sure whant to delete banner <b>"+btName+"</b>?",
+          new Ngn.Dialog.Confirm({
+            message: "Are you sure whant to delete banner <b>"+btName+"</b> to trash?",
             onOkClose: function () {
               document.location.href="/list/deleteConfirmed/"+$(this).get('data');
             }.bind(this)
           });
           $$('.dialog').setStyle('top', '35%');
         });
-  </script>
-<? }
-else { ?>
+    </script>
+  </div>
+<? } else { ?>
   <p>You have no banners</p>
 <? } ?>
+  <script>
+  function getTrash() {
+  new Ngn.Request({
+  url: '/list/getTrash',
+  onComplete: function(response) {
+  $$('#tablepace').set('html', response);
+  }
+  }).send();
+  }
+  $$('#trash').addEvent('click', function(event) {
+    restored=false;
+  new Ngn.Dialog({
+    title: 'Trash',
+    message: "<div id='tablepace' style='text-align: center;'></div>",
+    onClose: function () {
+      if(restored){
+        document.location.href="/list/";
+      }
+    },
+  });
+    $$('.table').setStyle('top', '15%');
+    $$('.dialog').setStyle('top', '15%');
+    $$('.dialog-message').setStyle('max-height', '400px').setStyle('overflow-y', 'auto');
+  getTrash();
+  });
+
+  function getCountTrash() {
+    new Ngn.Request({
+      url: "/list/getCountTrash",
+      onComplete: function(response) {
+        if(response>0){
+          $$('#trash_ctn').set('html', response);
+        } else {$$('#trash').setStyle('display', 'none');}
+      }
+    }).send();
+  }
+
+  function delTrash(id) {
+    var btName = $$('#name-'+id).get('text');
+
+    new Ngn.Dialog.Confirm({
+      message: "Are you sure you want to permanently delete <b>"+btName+"</b> banner?",
+      onOkClose: function () {
+        new Ngn.Request({
+          url: "/list/deleteConfirmedTrash/"+id,
+          onComplete: function(response) {
+              getTrash();
+              getCountTrash();
+          },
+        }).send();
+      }.bind(this)
+    });
+  };
+  function restoreTrash(id) {
+    var btName = $$('#name-'+id).get('text');
+
+    new Ngn.Dialog.Confirm({
+      message: "Are you sure restore banner <b>"+btName+"</b> from trash?",
+      onOkClose: function () {
+        new Ngn.Request({
+          url: "/list/restoreTrash/"+id,
+          onComplete: function(response) {
+            getTrash();
+            getCountTrash();
+            if(response=="Ok"){
+              restored=true;
+            }
+          },
+        }).send();
+      }.bind(this)
+    });
+  };
+  </script>
+</div>
 
 
