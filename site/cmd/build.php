bd8b9f78b588e463281ffa850bd2a5d0f8b550e4
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-09-07 12:50:57 +0300

    improve build script BC-9

--- site/cmd/build.php
+++ site/cmd/build.php
@@ -4,14 +4,25 @@ $currentBuildMode = BUILD_MODE;
 ProjectConfig::updateConstant('more', 'BUILD_MODE', true);
 print `php cmd.php cc`;
 
-file_get_contents('http://'.SITE_DOMAIN.'/cpanel/12?adminKey='.Config::getVar('adminKey'));
-file_get_contents('http://'.SITE_DOMAIN.'/newBanner');
-file_get_contents('http://'.SITE_DOMAIN.'/support');
-file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/111/json_blockSettings/348');
-file_get_contents('http://'.SITE_DOMAIN.'/pageBlock/111/json_blockSettings/349');
-file_get_contents('http://'.SITE_DOMAIN.'/cpanel/11/json_settings');
-file_get_contents('http://'.SITE_DOMAIN.'/admin/usersBcreator?adminKey='.Config::getVar('adminKey'));
-file_get_contents('http://'.SITE_DOMAIN.'/admin/ddItemsFilter/backgrounds/editContent?adminKey='.Config::getVar('adminKey'));
+$links = [
+  'http://'.SITE_DOMAIN.'/cpanel/111?adminKey='.Config::getVar('adminKey'),
+  'http://'.SITE_DOMAIN.'/newBanner',
+  'http://'.SITE_DOMAIN.'/support',
+  'http://'.SITE_DOMAIN.'/pageBlock/111/json_blockSettings/348',
+  'http://'.SITE_DOMAIN.'/pageBlock/111/json_blockSettings/349',
+  'http://'.SITE_DOMAIN.'/cpanel/111/json_settings?adminKey='.Config::getVar('adminKey'),
+  'http://'.SITE_DOMAIN.'/admin/usersBcreator?adminKey='.Config::getVar('adminKey'),
+  'http://'.SITE_DOMAIN.'/admin/ddItemsFilter/backgrounds/editContent?adminKey='.Config::getVar('adminKey'),
+];
+$curl = new Curl;
+foreach ($links as $link) {
+  $curl->check200AndThrow($link);
+}
 
-if ($currentBuildMode === false) ProjectConfig::updateConstant('more', 'BUILD_MODE', false);
+if ($currentBuildMode === false) {
+  ProjectConfig::updateConstant('more', 'BUILD_MODE', false);
+  foreach ($links as $link) {
+    $curl->check200AndThrow($link);
+  }
+}
 output('build complete');
