1fba3407b50dcc4138acda6f4c49f8ef0e275412
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2017-03-22 08:15:43 -0400

    remove bcreator stuff

--- site/init.php
+++ site/init.php
@@ -3,29 +3,3 @@
 require dirname(NGN_PATH).'/sd2/init.php';
 
 O::replaceInjection('DefaultRouter', 'BcreatorRouter');
-//O::replaceInjection('BcRender', 'BcreatorRender');
-//$root = dirname(dirname(dirname(__DIR__)));
-//Lib::addPsr4Folder($root.'/Library');
-//Lib::addPsr4Folder($root.'/Library/Engine/etc');
-//Lib::addPsr2Folder($root.'/Library');
-//Sql::setThrowErrors(true);
-//Lib::required('Db'); // require to load DB_* constants
-//Sql::connectDatabase([
-//  'host' => DB_HOST,
-//  'username' => DB_USER,
-//  'password' => DB_PASS,
-//  'dbname' => DB_NAME
-//]);
-
-//Sflm::$debugPaths = [
-//  'css' => [
-//    'design.css',
-//    'home.css',
-//    'table.css',
-//    'banners.css',
-//  ],
-//  'js'  => [
-//    'Ngn.Carousel'
-//  ]
-//];
-//
